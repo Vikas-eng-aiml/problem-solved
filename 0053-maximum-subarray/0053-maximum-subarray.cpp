f10bda2f402fb666e81b312c4c6395fc7dde9622
Vikas kumar yadav <vikaskkumar01122@gmail.com>
2025-03-26 00:41:18 +0530

    Time: 0 ms (100%), Space: 71.8 MB (53.26%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& arr) {
+        int maxi=INT_MIN;
+        int prefix=0;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            prefix+=arr[i];
+            maxi=max(prefix,maxi);
+            if(prefix<0)
+            prefix=0;
+        }
+        return maxi;
+    }
+};
